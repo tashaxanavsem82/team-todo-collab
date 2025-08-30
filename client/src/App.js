365ec3b75e6dc4a578e7bf68c8f08d592ccc1d27
tashaxanavsem82 <swgwull@hotmail.com>
2025-08-30 01:29:25 -0500

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Team Todo Collab</h1>
+      <p>Your collaborative todo list application.</p>
+    </div>
+  );
+}
+
+export default App;
